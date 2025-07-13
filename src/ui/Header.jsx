4aad5417c4e42fab0898e4fc48958a69e4ab75d3
Beridze0 <beridzedavit904@gmail.com>
2025-07-13 22:58:34 +0400

    styling text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Davit</p>
+      <Username />
     </header>
   );
 }
