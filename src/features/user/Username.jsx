b3f2c198a7f47612c96135ff67381c4f95c68e0a
Beridze0 <beridzedavit904@gmail.com>
2025-07-14 00:21:05 +0400

    using flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,3 @@
 export default function Username() {
-  return <div className="hidden text-sm font-semibold">Davit</div>;
+  return <div className="hidden text-sm font-semibold md:block">Davit</div>;
 }
